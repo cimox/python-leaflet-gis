6ad9d509e801657833fbabab5c4eedf22a5a6055
cimox <matus.cimerman@gmail.com>
2016-10-21 14:51:38 +0200

    Add fancy nearby tracks selecting

--- static/js/map.js
+++ static/js/map.js
@@ -3,11 +3,16 @@
  */
 
 $(function () {
-    var map;
+    // constants
+    var DEFAULT_TRACK_HEIGHT = 4.5, DEFAULT_TRACK_OPACITY = 0.75, DEFAULT_TRACK_COLOR = '#e57373';
+    var CLICK_TRACK_HEIGHT = 7, CLICK_TRACK_OPACITY = 1, CLICK_TRACK_COLOR = '#ef5350';
+
+    var map, marker, markerRadius;
+    var initPosition = [48.7392252, 18.9908871];
     initMap = function () {
         console.log('Map is ready.');
         map = L.map('map-view', {
-            center: [48.7392252, 18.9908871],
+            center: initPosition,
             zoom: 11
         });
 
@@ -15,7 +20,14 @@ $(function () {
             'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2ltb3giLCJhIjoiY2lnMmR5cTViMDBxanZza2hnMW52bnE4cCJ9.69Gz4ubptFwygdY_zA7c3Q'
         ).addTo(map);
 
-        L.marker([48.7392252, 18.9908871]).addTo(map);
+        marker = L.marker(initPosition, {draggable: true})
+            .addTo(map).bindPopup('Drag me around');
+
+        markerRadius = L.circle(initPosition, 2500)
+            .addTo(map);
+        marker.on('drag', function(event) {
+            markerRadius.setLatLng(marker.getLatLng());
+        });
 
         map.on('locationfound', onLocationFound);
         map.on('locationerror', onLocationError);
@@ -43,6 +55,7 @@ $(function () {
             elemLocation.toggleClass('enabled disabled');
         }
     }
+
     function onLocationError(e) {
         alert("Couldn't get your location!");
     }
@@ -71,7 +84,7 @@ $(function () {
                     return L.marker(latlng, {icon: hutIcon});
                 },
                 onEachFeature: function (feature, layer) {
-                    layer.bindPopup(feature.properties.title || 'Neznámy');
+                    layer.bindPopup(feature.properties.title || 'Unknown');
                 }
             });
             sheltersLayer.addTo(map);
@@ -80,11 +93,36 @@ $(function () {
         });
     };
 
+    var nearbyTracksLayer = null;
     findNearby = function () {
-        console.log('find nearby tracks');
-        $.getJSON('/nearby/48.7392252/18.9908871/10000/', function (geojson) {
+        if (nearbyTracksLayer != null) map.removeLayer(nearbyTracksLayer);
+        $.getJSON('/nearby/' + marker.getLatLng().lat + '/' + marker.getLatLng().lng + '/10000/', function (geojson) {
+            console.log(geojson);
             nearbyTracksLayer = L.geoJson(geojson, {
+                onEachFeature: function (feature, layer) {
+                    var title = feature.properties.title || 'Unknown track name';
+                    layer.setStyle({
+                        color: DEFAULT_TRACK_COLOR,
+                        opacity: DEFAULT_TRACK_OPACITY,
+                        weight: DEFAULT_TRACK_HEIGHT
+                    });
+                    layer.bindPopup(title  + ' | ' + feature.properties.length + 'm');
 
+                    layer.on('click', function (event) {
+                        layer.setStyle({
+                            color: CLICK_TRACK_COLOR,
+                            opacity: CLICK_TRACK_OPACITY,
+                            weight: CLICK_TRACK_HEIGHT
+                        });
+                    });
+
+                    layer.on('popupclose', function () {
+                        layer.setStyle({
+                            color: DEFAULT_TRACK_COLOR,
+                            opacity: DEFAULT_TRACK_OPACITY,
+                            weight: DEFAULT_TRACK_HEIGHT});
+                    });
+                }
             });
             nearbyTracksLayer.addTo(map);
 
