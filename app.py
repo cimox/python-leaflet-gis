6156adba14fa62c74bcb896af52ad501888974c9
cimox <matus.cimerman@gmail.com>
2016-10-27 21:06:05 +0200

    Add finding tracks near to any city within some distance

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from db_utils import Postis
 from flask import Flask, render_template
 
 app = Flask(__name__)
-db = Postis()
+db = Postis('postgres', 'postgres', 5432, 'localhost')
 
 @app.route('/')
 def hello_world() -> object:
@@ -13,14 +13,21 @@ def hello_world() -> object:
 
 @app.route('/shelters/')
 def api_shelters() -> dict:
-    shelters = db._get_shelters()
+    shelters = db.get_shelters()
     return geojson.dumps(shelters)
 
 
 @app.route('/nearby/<float:lat>/<float:lng>/<int:radius>/', methods=['GET'])
 def api_nearby(lat, lng, radius) -> dict:
-    nearby_tracks = db._get_nearby(lat, lng, radius)
+    nearby_tracks = db.get_nearby(lat, lng, radius)
     return geojson.dumps(nearby_tracks)
+
+
+@app.route('/nearby-city/<int:radius>/', methods=['GET'])
+def api_nearby_city(radius) -> dict:
+    nearby_city = db.get_nearby_city(radius)
+    return geojson.dumps(nearby_city)
+
 
 if __name__ == '__main__':
     app.run()
